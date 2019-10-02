cd4f86892193365e28cb88f7c1f1cdef0c6218bf
AceGentile <AceGentile@users.noreply.github.com>
2019-10-02 05:55:42 +0200

    Do not display "null" when values are set to null (#42)
    * js / main.js: fix language null
    
    fixes console error in case of repo.language.toLowercase()
    when repo.language is null and preventing 'null' as language
    name being displayed in repos list
    
    * prevent user data  being displayed as null
    
    prevent user.name, user.bio, user.location, user.company
    being displayed as 'null'

--- js/main.js
+++ js/main.js
@@ -180,7 +180,7 @@ $(document).ready(function(){
                     let desc = repo.desciption;
                     if (desc==null)
                         desc = "";
-                    let lang = repo.language.toLowerCase();
+                    let lang = (repo.language || '').toLowerCase();
                     switch (lang){
                         case "html":
                             lang = "html5";
@@ -202,7 +202,7 @@ $(document).ready(function(){
                                         <a target = "_blank" href="https://github.com/${username}/${repo.name}/network/members" class="collection-item"><span class="new badge" data-badge-caption="">${repo.forks_count}</span>Forks</a>
                                         <a target = "_blank" href="https://github.com/${username}/${repo.name}/watchers" class="collection-item"><span class="new badge" data-badge-caption="">${repo.watchers_count}</span>Watchers</a>
                                         <a target = "_blank" href="https://github.com/${username}/${repo.name}/stargazers" class="collection-item"><span class="new badge" data-badge-caption="">${repo.stargazers_count}</span>Stars</a>
-                                        <a target = "_blank" href="https://github.com/${username}/${repo.name}" class="collection-item"><span class="badge"><i class="devicon-${lang}-plain colored"></i>${repo.language}</span>Language</a>
+                                        <a target = "_blank" href="https://github.com/${username}/${repo.name}" class="collection-item"><span class="badge">${lang && `<i class="devicon-${lang}-plain colored"></i>`}${repo.language || '-'}</span>Language</a>
                                         <a target = "_blank" href="https://github.com/${username}/${repo.name}" class="collection-item"><span class="new badge" data-badge-caption="">${repo.created_at.substring(0,10)}</span>Created</a>
                                         <a target = "_blank" href="https://github.com/${username}/${repo.name}" class="collection-item"><span class="new badge" data-badge-caption="">${repo.updated_at.substring(0,10)}</span>Last Updated</a>
                                     </div>
@@ -218,7 +218,7 @@ $(document).ready(function(){
             });
             $('#profile').html(`
             <div class="col s12 m7">
-                <h4 class="header"><i class="small material-icons">account_box</i>${user.name}</h4>
+                <h4 class="header"><i class="small material-icons">account_box</i>${user.name || '-'}</h4>
                 <div class="card hoverable horizontal">
                 <div id="details" class="row container">
                 <div class="card-image col s12 m5">
@@ -227,9 +227,9 @@ $(document).ready(function(){
                 <div class="card-stacked col s12 m7">
                     <div class="card-content">
                     <div class="collection">
-                    <a target = "_blank" href="${user.html_url}" class="collection-item"><span class="badge">${user.bio}</span>Bio</a>
-                    <a target = "_blank" href="${user.html_url}" class="collection-item"><span class="badge">${user.company}</span>Company</a>
-                    <a target = "_blank" href="${user.html_url}" class="collection-item"><span class="badge">${user.location}</span>Location</a>
+                    <a target = "_blank" href="${user.html_url}" class="collection-item"><span class="badge">${user.bio || '-'}</span>Bio</a>
+                    <a target = "_blank" href="${user.html_url}" class="collection-item"><span class="badge">${user.company || '-'}</span>Company</a>
+                    <a target = "_blank" href="${user.html_url}" class="collection-item"><span class="badge">${user.location || '-'}</span>Location</a>
                     <a target = "_blank" href="${user.html_url}?tab=repositories" class="collection-item"><span class="new badge" data-badge-caption="">${user.public_repos}</span>Repos</a>
                     <a target = "_blank" href="https://gist.github.com/${user.login}" class="collection-item"><span class="new badge" data-badge-caption="">${user.public_gists}</span>Gists</a>
                     <a target = "_blank" href="${user.html_url}?tab=followers" class="collection-item"><span class="new badge" data-badge-caption="">${user.followers}</span>Followers</a>
